4806ed4b330930f473d1f94e82f88fe4da534812
GarDeN <garpinich22@mail.ru>
2023-01-18 12:08:56 +0300

    Deleted Galaxy.txt and added new int life

--- Life/Source.cpp
+++ Life/Source.cpp
@@ -1,111 +1,67 @@
 #include <iostream>
 #include <windows.h>
 #include <fstream>
-void life_cycle(char** galaxy_tab, char** galaxy_tab_buf, int n, int m, int* alive_cells, bool& flag);
-
+void life_cycle(char** galaxy_tab, char** galaxy_tab_buf, int galaxy_tab_height, int galaxy_tab_length, int* alive_cells, bool& flag);
+char** create_array(int x, int y);
+void delete_array(char** arr, int x);
+void print_array(char** arr, int x, int y);
 
 int main() {
-	std::fstream f("Galaxy.txt", std::ios::in | std::ios::out | std::ios::trunc);
-	std::ifstream fin("input.txt");
+	std::ifstream fin("input_life.txt");
 	if (fin.is_open()) {
-		int n, m;
-		fin >> n >> m;
-		char** galaxy_tab = new char* [n];
-		for (int i = 0; i < n; i++)
-			galaxy_tab[i] = new char[m];
-		char** galaxy_tab_buf = new char* [n];
-		for (int i = 0; i < n; i++)
-			galaxy_tab_buf[i] = new char[m];
-
-		if (f.is_open())
-		{
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					galaxy_tab[i][j] = '-'; 
-					galaxy_tab_buf[i][j] = '-'; // buffer table
-				}
+		int galaxy_tab_height, galaxy_tab_length;
+		fin >> galaxy_tab_height >> galaxy_tab_length;
+		char** galaxy_tab = create_array(galaxy_tab_height, galaxy_tab_length);
+		char** galaxy_tab_buf = create_array(galaxy_tab_height, galaxy_tab_length);
+		for (int i = 0; i < galaxy_tab_height; i++) {
+			for (int j = 0; j < galaxy_tab_length; j++) {
+				galaxy_tab[i][j] = '-';
+				galaxy_tab_buf[i][j] = '-'; // buffer table
 			}
-			int x, y;
-			int alive_cells = 0;
-			while (!(fin.eof())) {
-				alive_cells++;
-				fin >> x >> y;
-				galaxy_tab[x][y] = '*';
-				galaxy_tab_buf[x][y] = '*';
-			}
-
-			// fill in the Galaxy by init conditions
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					f << galaxy_tab[i][j] << ' ';
-				}
-				f << '\n';
-			}
-			f.seekg(0);
-			// print the first generation of the Galaxy
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					f >> galaxy_tab[i][j];
-					std::cout << galaxy_tab[i][j] << ' ';
-				}
-				std::cout << '\n';
-			}
-
-			int generation = 1;
-			std::cout << "Generation: " << generation << " Alive cells: " << alive_cells << '\n';
-			bool flag;
-			do {
-				Sleep(1000);
-				if (alive_cells == 0) {
-					std::cout << "All cells are dead. Game over";
-					break;
-				}
-				flag = true;
-				f.seekg(0);
-				life_cycle(galaxy_tab, galaxy_tab_buf, n, m, &alive_cells, flag);
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < m; j++) {
-						f << galaxy_tab[i][j] << ' ';
-					}
-					f << '\n';
-				}
-				f.seekg(0);
-				for (int i = 0; i < n; i++) {
-					for (int j = 0; j < m; j++) {
-						f >> galaxy_tab[i][j];
-						std::cout << galaxy_tab[i][j] << ' ';
-					}
-					std::cout << '\n';
-				}
-				generation++;
-				std::cout << "Generation: " << generation << " Alive cells: " << alive_cells << '\n';
-			} while (!flag);
-			if (alive_cells > 0)
-				std::cout << "The world has stagnated. Game over";
 		}
-		else
-			std::cout << "File Galaxy.txt isn't opened";
-		for (int i = 0; i < n; i++)
-			delete[] galaxy_tab[i];
-		delete[] galaxy_tab;
-		galaxy_tab = nullptr;
-		for (int i = 0; i < n; i++)
-			delete[] galaxy_tab_buf[i];
-		delete[] galaxy_tab_buf;
-		galaxy_tab_buf = nullptr;
+		int active_cell_x, active_cell_y; // input active cells in the galaxy_tab
+		int alive_cells = 0;
+		while (!(fin.eof())) {
+			alive_cells++;
+			fin >> active_cell_x >> active_cell_y;
+			galaxy_tab[active_cell_x][active_cell_y] = '*';
+			galaxy_tab_buf[active_cell_x][active_cell_y] = '*';
+		}
+		print_array(galaxy_tab, galaxy_tab_height, galaxy_tab_length);
+
+		int generation = 1;
+		std::cout << "Generation: " << generation << " Alive cells: " << alive_cells << '\n';
+		bool flag; // flag is needed to compare the new table (galaxy_tab_buf) with the old table (galaxy_tab) 
+		do {
+			Sleep(10);
+			if (alive_cells == 0) {
+				std::cout << "All cells are dead. Game over";
+				break;
+			}
+			flag = true;
+			life_cycle(galaxy_tab, galaxy_tab_buf, galaxy_tab_height, galaxy_tab_length, &alive_cells, flag);
+			print_array(galaxy_tab, galaxy_tab_height, galaxy_tab_length);
+			generation++;
+			std::cout << "Generation: " << generation << " Alive cells: " << alive_cells << '\n';
+		} while (!flag);
+		if (alive_cells > 0) {
+			std::cout << "The world has stagnated. Game over";
+		}
+		delete_array(galaxy_tab, galaxy_tab_height);
+		delete_array(galaxy_tab_buf, galaxy_tab_height);
+
 	}
 	else std::cout << "File input.txt isn't opened";
-	f.close();
 	fin.close();
 	return 0;
 }
 
 
-void life_cycle(char** galaxy_tab, char** galaxy_tab_buf, int n, int m, int* alive_cells, bool& flag) {
+void life_cycle(char** galaxy_tab, char** galaxy_tab_buf, int galaxy_tab_height, int galaxy_tab_length, int* alive_cells, bool& flag) {
 	int count_alive;
 	int count_dead;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+	for (int i = 0; i < galaxy_tab_height; i++) {
+		for (int j = 0; j < galaxy_tab_length; j++) {
 			if (galaxy_tab[i][j] == '-') {
 				count_alive = 0;
 				count_dead = -1;
@@ -114,9 +70,14 @@ void life_cycle(char** galaxy_tab, char** galaxy_tab_buf, int n, int m, int* ali
 				count_alive = -1;
 				count_dead = 0;
 			}
+			// check every cell around a chosen cell
+			// k = i - 1; k < i + 2 and l = j - 1; l < j + 2 are used for checking 8 cells around a chosen cell
+			// if k = -1 or l <= -1 or k = i + 1 or l = j + 1 then the cell in the corner of the galaxy_tab is checking
 			for (int k = i - 1; k < i + 2; k++) {
+				if (k < 0)
+					continue;
 				for (int l = j - 1; l < j + 2; l++) {
-					if (k >= 0 && k < n && l >= 0 && l < m) {
+					if (k >= 0 && k < galaxy_tab_height && l >= 0 && l < galaxy_tab_length) {
 						if (galaxy_tab[k][l] == '-') {
 							count_dead++;
 						}
@@ -136,12 +97,35 @@ void life_cycle(char** galaxy_tab, char** galaxy_tab_buf, int n, int m, int* ali
 			}
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+	for (int i = 0; i < galaxy_tab_height; i++) {
+		for (int j = 0; j < galaxy_tab_length; j++) {
 			if (galaxy_tab[i][j] != galaxy_tab_buf[i][j]) {
 				galaxy_tab[i][j] = galaxy_tab_buf[i][j];
 				flag = false;
 			}
 		}
+	}
+}
+
+char** create_array(int x, int y) {
+	char** arr = new char* [x];
+	for (int i = 0; i < x; i++)
+		arr[i] = new char[y];
+	return arr;
+}
+
+void delete_array(char** arr, int x) {
+	for (int i = 0; i < x; i++)
+		delete[] arr[i];
+	delete[] arr;
+	arr = nullptr;
+}
+
+void print_array(char** arr, int x, int y) {
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < y; j++) {
+			std::cout << arr[i][j] << ' ';
+		}
+		std::cout << '\n';
 	}
 }
